38d4c9f945864504a999c5e49c2388db770632ae
unknown <judemin2087@naver.com>
2024-08-02 13:27:45 +0900

    [INIT] : Initialized Project

---
+++ src/main/java/org/example/traflixspringbe/TraflixSpringBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.traflixspringbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraflixSpringBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraflixSpringBeApplication.class, args);
+    }
+
+}
